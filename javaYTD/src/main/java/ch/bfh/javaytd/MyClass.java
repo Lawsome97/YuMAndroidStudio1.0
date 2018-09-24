66405cc85fa8f326bce6636fd98bed0ab376b186
Lawsome97 <schaa9@bfh.ch>
2018-09-24 10:06:24 +0200

    Corrected

--- javaYTD/src/main/java/ch/bfh/javaytd/MyClass.java
+++ javaYTD/src/main/java/ch/bfh/javaytd/MyClass.java
@@ -4,22 +4,19 @@ import java.io.PrintWriter;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello Java World");
 
         String download_path="C:\\Users\\anina\\OneDrive\\Dokumente\\Youtube-dl";
-        String url="https://www.youtube.com/watch?v=F2Wm4Dh5yCk";
-        String[] command =
-                {
-                        "cmd",
-                };
+        String destination_path= "C:\\Users\\anina\\Music";
+        String url="";
+
         Process p;
         try {
-            p = Runtime.getRuntime().exec(command);
+            p = Runtime.getRuntime().exec("cmd");
             new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
             new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
             PrintWriter stdin = new PrintWriter(p.getOutputStream());
             stdin.println("cd \""+download_path+"\"");
-            stdin.println(download_path+"\\youtube-dl "+url);
+            stdin.println(download_path+"\\youtube-dl   --extract-audio    --audio-format mp3  --audio-quality 0  "+ url);
             stdin.close();
             p.waitFor();
         } catch (Exception e) {
